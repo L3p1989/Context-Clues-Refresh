fd410983939ec0240f6051d3364416fca04366a0
L3p1989 <phogan110@live.com>
2021-03-20 15:55:15 -0700

    created array of locations

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 //array of friends
 var friends = ["Glenn", "Julius", "Carissa", "Siacca", "Andi"];
+//Array of locations
+var locations = [
+  "Living Room",
+  "Bathroom",
+  "Bedroom",
+  "Hallway",
+  "Stair Case",
+  "Entry Way",
+  "Kitchen",
+  "Dining Room",
+  "Pantry",
+  "Basement",
+];
